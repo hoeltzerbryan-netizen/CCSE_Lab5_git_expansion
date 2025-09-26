b8da539a4a33b5fc73cc28140ac7b6527cde9ed7
hoeltzerbryan-netizen <hoeltzerbryan@gmail.com>
2025-09-26 10:44:05 -0400

    Finished Account file
    Finished the rest of the code for the account file.

--- Account.java
+++ Account.java
@@ -1,30 +1,44 @@
 public class Account {
+    //initializing 3 vars that will be used in the rest of the project like account#, bal#,
+    //and var to hold next account number
     static private int accountNumber;
     private int nextNumber =10001;
     private double accountBalance;
 
+    //default constructor
     void Account(){
         accountNumber = nextNumber;
         nextNumber++;
         accountBalance = 0.0;
     }
+
+    //constructor for an account with  bal
     void Account(double i){
         accountNumber = nextNumber;
         nextNumber++;
         accountBalance = i;
     }
+
+    //Just subtracting balance
     double withdraw(double i){
         accountBalance = accountBalance - i;
         return accountBalance;
     }
+
+    //adding to bal
     double deposit(double i){
-        return i;
+        accountBalance = accountBalance + i;
+        return accountBalance;
     }
+
+    //returns the balance
     double getAccountBalance(){
-        return ;
+        return accountBalance;
     }
+
+    //return account #
     double getAccountNumber(){
-        return ;
+        return accountNumber;
     }
     @Override
     public String toString(){
