4c5896204d06e8b25dcdc9f3f3cda7ef11986200
hoeltzerbryan-netizen <hoeltzerbryan@gmail.com>
2025-09-26 10:37:02 -0400

    Some work to the account class
    basic methods being defined like initializing accounts and balance and basic methods with them

--- Account.java
+++ Account.java
@@ -1,3 +1,33 @@
 public class Account {
+    static private int accountNumber;
+    private int nextNumber =10001;
+    private double accountBalance;
 
+    void Account(){
+        accountNumber = nextNumber;
+        nextNumber++;
+        accountBalance = 0.0;
+    }
+    void Account(double i){
+        accountNumber = nextNumber;
+        nextNumber++;
+        accountBalance = i;
+    }
+    double withdraw(double i){
+        accountBalance = accountBalance - i;
+        return accountBalance;
+    }
+    double deposit(double i){
+        return i;
+    }
+    double getAccountBalance(){
+        return ;
+    }
+    double getAccountNumber(){
+        return ;
+    }
+    @Override
+    public String toString(){
+        return "Account "+accountNumber+", balance $"+accountBalance;
+    }
 }
