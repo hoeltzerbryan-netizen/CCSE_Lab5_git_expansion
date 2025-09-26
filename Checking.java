37c70993cca0131a7267e7ab3acd6cc4868cb1b8
hoeltzerbryan-netizen <hoeltzerbryan@gmail.com>
2025-09-26 13:35:17 -0400

    Finished Checking branch
    Finished withdraw method and toString Might have to come back to constructor

--- Checking.java
+++ Checking.java
@@ -1,10 +1,22 @@
 public class Checking extends Account {
     Checking(double i){
-
+        super(i);
     }
 
     @Override
     double withdraw(double i){
-        
+        accountBalance = accountBalance - i;
+        if(getAccountBalance()<0){
+            accountBalance = accountBalance - 20;
+            return "“Charging an overdraft fee of $20 because " +
+                    "account is below $0";
+        }else{
+            return accountBalance;
+        }
+
+    }
+    @Override
+    public String toString(){
+        return "Account #" + getAccountNumber()+", balance $"+ getAccountBalance;
     }
 }
