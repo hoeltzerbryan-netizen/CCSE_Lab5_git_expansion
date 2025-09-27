b642758372e91c83ef040fb8b766b098d4b7403a
hoeltzerbryan-netizen <hoeltzerbryan@gmail.com>
2025-09-26 21:12:52 -0400

    Halfway done with main file lab5
    Finishing the switch option cases

--- Lab5.java
+++ Lab5.java
@@ -3,6 +3,8 @@ class Lab5{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int option;
+        Checking C = new Checking(0);
+        Savings S = new Savings(500);
         System.out.print("[Banking System]");
         do{
             System.out.print("1. Withdraw from Checking\n" +
@@ -15,6 +17,41 @@ class Lab5{
                     "8. Quit\n" +
                     "Select option: ");
             option = sc.nextInt();
+
+            switch(option){
+                double numb;
+                case 1:
+                    System.out.println("How much would you like to withdraw from Checking? $");
+                    numb = sc.nextDouble();
+                    C.withdraw(numb);
+                    if(C.getAccountBalance()<0){
+                        System.out.print("Charging an overdraft fee of $20 because account is below $0");
+                    }
+                    System.out.print("Current balance of Checking is $"+C.getAccountBalance());
+                    break;
+                case 2:
+                    System.out.print("How much would you like to withdraw from Savings? $");
+                    numb = sc.nextDouble();
+                    S.withdraw(numb);
+                    if(S.getAccountBalance()<0){
+                        System.out.print("Charging an overdraft fee of $10 because you are below $500");
+                    }
+                    System.out.print("Current balance of Checking is $"+S.getAccountBalance());
+                    break;
+                case 3:
+                    System.out.print("How much would you like to deposit to Checking");
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.print("Invalid option.");
+            }
         }while(option !=8);
         System.out.print("hi");
     }
