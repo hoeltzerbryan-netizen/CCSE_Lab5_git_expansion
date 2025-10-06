f533719a4675d27ca37b72c57b551758dd1422e9
hoeltzerbryan-netizen <hoeltzerbryan@gmail.com>
2025-10-06 13:54:28 -0400

    Finished password file
    Added a password feature to my bank account so now I can check to make sure this user is who they say they are.

--- Password.java
+++ Password.java
@@ -7,4 +7,21 @@ public class Password extends Account {
         accountUser = "";
         password = "";
     }
+    Password(String u, String p,double i) {
+        super(i);
+        accountUser = u;
+        password = p;
+    }
+    Boolean isPass(String p){
+        if(password.equals(p))
+            return true;
+        return false;
+    }
+    String getAccountUser(){
+        return accountUser;
+    }
+
+    public String toString(){
+        return "correct password continue!";
+    }
 }
